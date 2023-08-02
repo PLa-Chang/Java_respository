8146c33b39f34ccafe4031e8fbee99a2433a5442
王福昌 <13192377+wang-fuchang@user.noreply.gitee.com>
2023-08-02 13:18:50 +0000

    add java-day03/Exer02.java. 交换两个数位置
    Signed-off-by: 王福昌 <13192377+wang-fuchang@user.noreply.gitee.com>

---
+++ java-day03/Exer02.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Exer02{
+
+	public static void main(String[] args){
+		/*
+			交换两个数位置
+			1. 输入两个数字
+		*/
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("请输入一个数字：");
+		int num = sc.nextInt();
+		System.out.println("请输入另一个数字：");
+		int num1 = sc.nextInt();
+		
+		System.out.printf("输入的两个数字分别是：%d , %d \r\n", num, num1);
+		
+		// 交换位置
+        // 第一种方法：异或法
+		// num = num ^ num1;
+		// num1 = num ^ num1;
+		// num = num ^ num1;
+		
+        //第二种方法：加法方法
+		// num = num + num1;
+		// num1 = num - num1;
+		// num = num - num1;
+		
+        
+		int temp = num;
+		num = num1;
+		num1 = temp;
+		
+		System.out.printf("交换位置后两个数字分别是：%d , %d", num, num1);
+	}
+}
