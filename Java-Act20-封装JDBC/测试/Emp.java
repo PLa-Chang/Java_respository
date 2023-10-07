c0607d992bf60b596abedbb223dc75914e078f6a
流觞 <86483506+PLa-Chang@users.noreply.github.com>
2023-10-07 18:01:21 +0800

    数据库实体类

---
+++ Java-Act20-封装JDBC/测试/Emp.java
@@ -0,0 +1,103 @@
+package JDBC.day01.pojo;
+
+import java.sql.Date;
+
+/**
+ * Emp 实体类
+ */
+public class Emp {
+    private Integer empno;
+
+    private String ename;
+
+    private String job;
+
+    private Integer mgr;
+
+    private Date hiredate;
+
+    private Double salary;
+
+    private Double comm;
+
+    private Integer deptno;
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+
+    public Double getComm() {
+        return comm;
+    }
+
+    public void setComm(Double comm) {
+        this.comm = comm;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "empno=" + empno +
+                ", ename='" + ename + '\'' +
+                ", job='" + job + '\'' +
+                ", mgr=" + mgr +
+                ", hiredate=" + hiredate +
+                ", salary=" + salary +
+                ", comm=" + comm +
+                ", deptno=" + deptno +
+                '}';
+    }
+}
